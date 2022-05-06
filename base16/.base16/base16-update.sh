7e8f6df80a3e0c5a4e0050e98a2ef89573b2c8bd
M3NIX <julian@ortel.tech>
2022-05-06 17:13:32 +0200

    workaround not needed because is fixed in template file already

--- base16/.base16/base16-update.sh
+++ base16/.base16/base16-update.sh
@@ -13,13 +13,7 @@ pybase16 inject -s $theme -f ~/.bash_theme && . ~/.bash_theme
 pybase16 inject -s $theme -f ~/.config/i3status/config
 pybase16 inject -s $theme -f ~/.config/i3/config && i3-msg reload &> /dev/null
 pybase16 inject -s $theme -f ~/.Xresources.d/colors && xrdb ~/.Xresources
-
 pybase16 inject -s $theme -f ~/.config/rofi/base16.rasi
-# workaround to fix rofi theme
-echo "element-text, element-icon {" >> ~/.config/rofi/base16.rasi
-echo "    background-color: inherit;" >> ~/.config/rofi/base16.rasi
-echo "    text-color:       inherit;" >> ~/.config/rofi/base16.rasi
-echo "}" >> ~/.config/rofi/base16.rasi
 
 # set theme in vim
 echo "colorscheme base16-$theme" > ~/.vim/colorscheme.vim
