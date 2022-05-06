8963b30c4bd35c5109be0624604f6ef47bcad2e7
M3NIX <julian@ortel.tech>
2022-05-06 20:54:36 +0200

    added wallpaper

--- cleanup.sh
+++ cleanup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-STOW_FOLDERS=(bash base16 git i3 lxterminal rofi template vim x)
+STOW_FOLDERS=(bash base16 git i3 lxterminal rofi template vim wallpaper x)
 
 for i in "${STOW_FOLDERS[@]}"
   do stow -D "$i" &> /dev/null && echo "unstowed $i"
