4b8af77727d76cd4a923391bf8bc8a44f4d0389b
M3NIX <julian@ortel.tech>
2022-05-08 12:39:54 +0200

    put all options of neovim in one place

--- neovim/.config/astronvim/lua/user/init.lua
+++ neovim/.config/astronvim/lua/user/init.lua
@@ -15,27 +15,23 @@ local config = {
       number = true,
       cursorline = true,
       cursorcolumn = true,
+      wrap = false,
       scrolloff = 8,
       colorcolumn = "100",
       signcolumn = "yes",
       -- search settings
       smartcase = true,
       incsearch = true,
+      hlsearch = false,
+      -- swap and undo settings
+      swapfile = false,
+      backup = false,
       -- misc
       termguicolors = true,
     },
     g = {
       mapleader = " ", -- sets vim.g.mapleader
     },
-    o = {
-      -- visuals
-      wrap = false,
-      -- search settings
-      hlsearch = false,
-      -- swap and undo settings
-      swapfile = false,
-      backup = false,
-    }
   },
 
 }
