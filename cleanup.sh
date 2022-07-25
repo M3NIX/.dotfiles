520b170663db7a7a747ae40933d9beb9fcfeec0e
M3NIX <julian@ortel.tech>
2022-07-25 16:30:18 +0200

    added compositor for transparent terminal

--- cleanup.sh
+++ cleanup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-STOW_FOLDERS=(bash base16 git i3 lxterminal neovim rofi template vim wallpaper x)
+STOW_FOLDERS=(bash base16 git i3 lxterminal neovim picom rofi template vim wallpaper x)
 
 for i in "${STOW_FOLDERS[@]}"
   do stow -D "$i" &> /dev/null && echo "unstowed $i"
