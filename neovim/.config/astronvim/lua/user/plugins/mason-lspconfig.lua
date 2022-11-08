a5907d8fe0c7abccf5a030f708114a836ceaeec8
M3NIX <julian@ortel.tech>
2022-11-08 16:41:30 +0100

    switched to mason

---
+++ neovim/.config/astronvim/lua/user/plugins/mason-lspconfig.lua
@@ -0,0 +1,20 @@
+return {
+  automatic_installation = true,
+  ensure_installed = {
+    "ansiblels",
+    "asm_lsp",
+    "bashls",
+    "clangd",
+    "cssls",
+    "dockerls",
+    "gopls",
+    "html",
+    "jsonls",
+    "lemminx",
+    "sumneko_lua",
+    "marksman",
+    "pyright",
+    "terraformls",
+    "yamlls"
+  }
+}
