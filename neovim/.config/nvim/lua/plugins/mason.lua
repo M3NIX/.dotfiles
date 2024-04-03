41dbbb61093f7778f57b53df5de9e711bcec5be3
M3NIX <julian@ortel.tech>
2024-04-03 16:20:42 +0200

    adding personal astronvim settings

--- neovim/.config/nvim/lua/plugins/mason.lua
+++ neovim/.config/nvim/lua/plugins/mason.lua
@@ -11,7 +11,20 @@ return {
     opts = function(_, opts)
       -- add more things to the ensure_installed table protecting against community packs modifying it
       opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
-        "lua_ls",
+        "ansiblels",
+        -- "asm_lsp",
+        "bashls",
+        "clangd",
+        "cssls",
+        "dockerls",
+        "gopls",
+        "jsonls",
+        "lemminx",
+        "luas_ls",
+        "marksman",
+        "pyright",
+        "terraformls",
+        "yamlls"
         -- add more arguments for adding more language servers
       })
     end,
--- neovim/.config/nvim/lua/plugins/treesitter.lua
+++ neovim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,5 +1,3 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- Customize Treesitter
 
 ---@type LazySpec
@@ -8,8 +6,28 @@ return {
   opts = function(_, opts)
     -- add more things to the ensure_installed table protecting against community packs modifying it
     opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
+      "bash",
+      "bibtex",
+      "c",
+      "cmake",
+      "comment",
+      "cpp",
+      "css",
+      "dockerfile",
+      "go",
+      "hcl",
+      "html",
+      "http",
+      "javascript",
+      "json",
       "lua",
+      "make",
+      "markdown",
+      "python",
+      "regex",
+      "toml",
       "vim",
+      "yaml",
       -- add more arguments for adding more treesitter parsers
     })
   end,
