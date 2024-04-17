de98182350c4315c2484fb2c1747a2b10c01174e
M3NIX <julian@ortel.tech>
2024-04-17 14:30:03 +0200

    adjusting scolling in neovim

--- neovim/.config/nvim/lua/plugins/astrocore.lua
+++ neovim/.config/nvim/lua/plugins/astrocore.lua
@@ -9,17 +9,25 @@ return {
     options = {
       opt = { -- vim.opt.<key>
         colorcolumn = "150",
+        scrolloff = 8,
       },
     },
     mappings = {
       n = {
         -- avic keybindings https://gist.github.com/M3NIX/36ad2581256a6a017b42565d4d652602
         ["<Leader>v"] = { desc = "ansible-vault" },
-        ["<leader>ve"] = { ":w | silent execute \"! avic -e -f % > /tmp/avic.tmp && mv /tmp/avic.tmp %\"<CR>",
-          desc = "ansible-vault inline encrypt" },
-        ["<leader>vd"] = { ":w | silent execute \"! avic -d -f % > /tmp/avic.tmp && mv /tmp/avic.tmp %\"<CR>",
-          desc = "ansible-vault inline decrypt" },
-    }
+        ["<leader>ve"] = {
+          ':w | silent execute "! avic -e -f % > /tmp/avic.tmp && mv /tmp/avic.tmp %"<CR>',
+          desc = "ansible-vault inline encrypt",
+        },
+        ["<leader>vd"] = {
+          ':w | silent execute "! avic -d -f % > /tmp/avic.tmp && mv /tmp/avic.tmp %"<CR>',
+          desc = "ansible-vault inline decrypt",
+        },
+        -- better movement
+        ["<C-d>"] = { "<C-d>zz", desc = "move down 5 lines" },
+        ["<C-u>"] = { "<C-u>zz", desc = "move down 5 lines" },
+      },
     },
   },
 }
